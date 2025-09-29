2f1143e11cae3f0969bdef4635b3c5e4e7f284d0
rajendra-chimala <rajendrachimala5@gmail.com>
2025-09-29 07:55:29 +0545

    commit

--- Array and Strings/01_Array.java
+++ Array and Strings/01_Array.java
@@ -3,7 +3,10 @@ package Array and Strings;
 public class 01_Array {
     public static void main(String[] args){
         System.out.println("Rajendra Chimala");
-        
+
+        int[] arr = {1,2,3,4}
+        System.out.print(arr)
+         
     }
     
 }
