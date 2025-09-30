ddfc602350eca14b4d05b150a34a3a921df1f966
rajendra-chimala <rajendrachimala5@gmail.com>
2025-09-30 19:48:37 +0545

    commit

--- Array and Strings/02_String.java
+++ Array and Strings/02_String.java
@@ -1,6 +1,4 @@
-public class 02_String{
-    public static void main(String[] arg){
-        String name = "Rajendra Chimala";
-        System.out.print(name);
-    }
-}
+public class 02_ String{public static void main(String[]arg){String name="Rajendra Chimala";System.out.print(name);
+
+// String is the collenction of char
+}}
